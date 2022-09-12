2921816dc12141b0950ac687fa0d8ca74576078e
feittosaa <lucas.feitosa2004@gmail.com>
2022-09-12 16:32:44 -0300

    modulo1

---
+++ Modulo1/Modulo1.py
@@ -0,0 +1,64 @@
+# algoritmo
+
+print("Hello World")
+
+# int (-1, 0, 1)
+
+# float (1.1, 0.2, 1.3)
+
+# complex (1+2j)
+
+x = 5
+y = 3.5
+z = 1 + 2.0j
+
+w = "Ana"
+w = "Ana"
+
+h, j, l = 1, 3, 5
+
+# atribui variaveis em sequencia
+print(h)
+print(j)
+print(l)
+
+a = b = c = " pYtHOn"
+
+# atribui um valor unico a varias variaveis
+print(a)
+print(b)
+print(c)
+
+# concatenar
+p = a + b
+print(p)
+
+# soma matematica
+q = h + j
+print(q)
+
+print(b.strip())
+print(b.lower())
+print(b.upper())
+
+print(type(a))
+print(type(q))
+
+
+#casting - conversao
+x = int(2)
+y = int(2.8)
+z = int('2')
+print(x, y, z)
+
+a = float(2)
+b = float(2.8)
+c = float('2')
+print(a, b, c)
+
+t = str(2)
+r = str(2.8)
+z = str('2')
+print("A variavel t eh do tipo: ", type(t))
+print("A variavel r eh do tipo: ", type(r))
+print("A variavel z eh do tipo: ", type(z))
